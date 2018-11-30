5943706bb0aae837401707f5bb9b29c01c7a3a3d
lana <sitdownrightnow2552@gmail.com>
2018-11-30 19:29:57 +0700

    fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RedirectorComponent} from './redirector/redirector.component';
 import {TodoComponent} from './todo/todo.component';
+import {BlogEditComponent} from './blog/blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog/blog-detail/blog-detail.component';
+import {BlogComponent} from './blog/blog/blog.component';
 
 const routes: Routes = [
   {path: '', component: RedirectorComponent},
-  {path: 'todo', component: TodoComponent}
+  {path: 'todo', component: TodoComponent}, {
+    path: 'blog',
+    component: BlogComponent
+  }, {
+    path: 'blog/:id',
+    component: BlogDetailComponent
+  }, {
+    path: 'blog/:id/edit',
+    component: BlogEditComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TodoComponent} from './todo/todo.component';
-import { RedirectorComponent } from './redirector/redirector.component';
+import {RedirectorComponent} from './redirector/redirector.component';
+import {BlogEditComponent} from './blog/blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog/blog-detail/blog-detail.component';
+import {BlogComponent} from './blog/blog/blog.component';
+import {TokenInterceptor} from './blog/token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
+    BlogComponent,
+    BlogDetailComponent,
+    BlogEditComponent,
     RedirectorComponent
   ],
   imports: [
@@ -21,7 +28,8 @@ import { RedirectorComponent } from './redirector/redirector.component';
     ReactiveFormsModule
   ],
   providers: [
-    {provide: 'TODO_API_URL', useValue: 'http://jsonplaceholder.typicode.com/todos'}
+    {provide: 'TODO_API_URL', useValue: 'http://jsonplaceholder.typicode.com/todos'},
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
